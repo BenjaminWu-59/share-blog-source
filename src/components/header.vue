1a08ad336124aa6abfc931ee492e152ec201f0c6
Benjamin <wuqiong3699@gmail.com>
2021-08-23 10:42:43 +0800

    夜间模式日渐模式完成

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,12 +1,12 @@
 <template>
-  <header :class="{login: isLogin, 'no-login': !isLogin}">
+  <header :class="{'login': isLogin, 'no-login': !isLogin,'isLight':light}">
     <template v-if="!isLogin">
       <h1>Let's share</h1>
       <p>精品博客汇聚</p>
       <div class="btns">
         <router-link to="/login"><el-button >立即登录</el-button></router-link>
         <router-link to="/register"><el-button>注册账号</el-button></router-link>
-      </div> 
+      </div>
     </template>
     <template v-if="isLogin">
       <h1><router-link to="/">Let's share</router-link></h1>
@@ -16,22 +16,25 @@
         <ul>
           <li><router-link to="/my">我的</router-link></li>
           <li><a href="#" @click="onLogout">注销</a></li>
+          <li v-if="light === false" @click="onNight">夜间</li>
+          <li v-if="light === true" @click="onDaytime">日间</li>
         </ul>
-      </div>         
+      </div>
     </template>
   </header>
 </template>
 
 <script>
-  
-  import auth from '@/api/auth'
+  import auth from '../api/auth.js'
   window.auth = auth
 
   import { mapGetters, mapActions } from 'vuex'
 
   export default {
     data() {
-      return {}
+      return {
+        light:false
+      }
     },
 
     computed: {
@@ -53,6 +56,13 @@
 
       onLogout() {
         this.logout()
+      },
+
+      onNight(){
+        this.light = true
+      },
+      onDaytime(){
+        this.light = false
       }
     }
 
@@ -62,7 +72,7 @@
 
 <style lang="less">
 
-@import "../assets/base.less";  
+@import "../assets/base.less";
 
 header.no-login {
   padding: 0 12% 30px 12%;
@@ -81,7 +91,7 @@ header.no-login {
     margin: 15px 0 0 0;
     color: #fff;
   }
-  
+
   .btns {
     margin-top: 20px;
   }
@@ -89,7 +99,7 @@ header.no-login {
   button {
     margin: 20px 5px 0;
   }
-} 
+}
 
 
 header.login {
@@ -134,18 +144,20 @@ header.login {
       margin:0;
       padding: 0;
       background-color: #fff;
+     li {
+       font-size: 12px;
+       display: block;
+       padding: 5px 10px;
+       cursor: pointer;
+       &:hover{
+        background: #eaeaea;
+       }
 
-      a {
-        text-decoration: none;
-        color: #333;
-        font-size: 12px;
-        display: block;
-        padding: 5px 10px;
-
-        &:hover {
-          background-color: #eaeaea;
-        }
-      }
+       a {
+         text-decoration: none;
+         color: #333;
+       }
+     }
 
     }
 
@@ -153,6 +165,9 @@ header.login {
       display: block;
     }
   }
+}
+header.isLight{
+  background: #3b3b3b;
 }
 
 
